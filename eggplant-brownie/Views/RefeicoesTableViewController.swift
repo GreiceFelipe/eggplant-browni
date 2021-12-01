cf55941303fea8c5f70b80406e9ef7895ff692f4
Greice Felipe <greice.n.felipe@gmail.com>
2021-12-01 08:45:23 -0300

    Adiciona tabela de refeições e navegação.

---
+++ eggplant-brownie/Views/RefeicoesTableViewController.swift
@@ -0,0 +1,28 @@
+//
+//  RefeicoesTableViewController.swift
+//  eggplant-brownie
+//
+//  Created by Greice Felipe on 01/12/21.
+//
+
+import UIKit
+
+class RefeicoesTableViewController: UITableViewController {
+    let refeicoes = [Refeicao(nome: "Macarrão", felicidade: 4),
+                     Refeicao(nome: "Pizza", felicidade: 4),
+                     Refeicao(nome: "Comida Japonesa", felicidade: 5)]
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        refeicoes.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celula = UITableViewCell(style: .default, reuseIdentifier: nil)
+        let refeicao = refeicoes[indexPath.row]
+        
+        celula.textLabel?.text = refeicao.nome
+        
+        return celula
+    }
+}
+
